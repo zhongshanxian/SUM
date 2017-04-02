66e7e8ffb93014760ceced3a10d81f6bb8443ac2
zhongshanxian <971296739@qq.com>
2017-04-02 18:08:10 +0800

    change

--- magnify/javascript/magnify.js
+++ magnify/javascript/magnify.js
@@ -4,41 +4,53 @@ window.onload = function(){
 	var floatBox = document.getElementById('floatBox');
 	var bigBox = document.getElementById('bigBox');
 	var bigImg = bigBox.getElementsByTagName('img')[0];
+	var mark = document.getElementById('mark');
+	
+	mark.onmouseover = function(){//鼠标放上去，显示floatbox和右边图片
 
-	smallBox.onmouseover = function(){//鼠标放上去，显示floatbox和右边图片
+		
+
 		floatBox.style.display = "block";
 		bigBox.style.display = "block";
 	};
 
-	smallBox.onmouseout = function(){//鼠标移开则隐藏
+	mark.onmouseout = function(){//鼠标移开则隐藏
+
+
+
 		floatBox.style.display = "none";
 		bigBox.style.display = "none";
 	};
 
-	smallBox.onmousemove = function(e){
-		var event1 = e;//获取当前位置
-		var left = event1.clientX - magnify.offsetLeft - smallBox.offsetLeft - floatBox.offsetWidth/2;//获取floatbox左边到小图片左边的距离
-		var top = event1.clientY - magnify.offsetTop - smallBox.offsetTop - floatBox.offsetHeight/2;//获取floatbox上边到小图片上边的距离
+	mark.onmousemove = function(e){
+		//var event1 = e;//获取当前位置
+
+		//调试兼容问题
+		//大部分用户使用ie浏览器
+		//上面获取e的方法不适用于ie
+		var event1 = e||window.event;
+		var left = event1.clientX - magnify.offsetLeft - mark.offsetLeft - floatBox.offsetWidth/2;//获取floatbox左边到小图片左边的距离
+		var top = event1.clientY - magnify.offsetTop - mark.offsetTop - floatBox.offsetHeight/2;//获取floatbox上边到小图片上边的距离
 
 		if (left<0) {//如果距离小于0，赋值为0
 			left=0;
 		}
-		else if (left>smallBox.offsetWidth - floatBox.offsetWidth) {//如果floatbox右边到小图片右边的距离小于0，赋值为最大距离
-			left=smallBox.offsetWidth - floatBox.offsetWidth;
+		else if (left>mark.offsetWidth - floatBox.offsetWidth) {//如果floatbox右边到小图片右边的距离小于0，赋值为最大距离
+			left=mark.offsetWidth - floatBox.offsetWidth;
 		}
 
 		if (top<0) {
 			top=0;
 		}
-		else if (top>smallBox.offsetHeight - floatBox.offsetHeight) {
-			top=smallBox.offsetHeight - floatBox.offsetHeight;
+		else if (top>mark.offsetHeight - floatBox.offsetHeight) {
+			top=mark.offsetHeight - floatBox.offsetHeight;
 		}
 
 		floatBox.style.left = left+"px";//设置floatbox的左边距离就是left
 		floatBox.style.top = top+"px";//上边距离就是top
 
-		var percentx = left/(smallBox.offsetWidth - floatBox.offsetWidth);//获取百分比，左边的距离/（左右两边剩余的空白距离）
-		var percenty = top/(smallBox.offsetHeight - floatBox.offsetHeight);
+		var percentx = left/(mark.offsetWidth - floatBox.offsetWidth);//获取百分比，左边的距离/（左右两边剩余的空白距离）
+		var percenty = top/(mark.offsetHeight - floatBox.offsetHeight);
 
 		bigImg.style.left = -percentx*(bigImg.offsetWidth-bigBox.offsetWidth)+"px";//负号表示反方向移动，移动距离=百分比*（大图片与显示框的空白差）
 		bigImg.style.top = -percenty*(bigImg.offsetHeight-bigBox.offsetHeight)+"px";
