4b9a8d001b8f30d4aa3de5f49f30a658991ba3f9
zhongshanxian <971296739@qq.com>
2017-08-07 22:57:12 +0800

    update 2048

--- 2048/javascript/2048.js
+++ 2048/javascript/2048.js
@@ -2,6 +2,10 @@ var board=new Array();
 var score=0;
 var hasConflicted=[];//记录是否发生过合并
 
+var startx=0,starty=0;
+var endx=0,endy=0;
+
+
 $(document).ready(function(){
 	prepareForMobile();
 	newgame();
@@ -166,9 +170,11 @@ function generateOneNumber(){
 
 ///判断按键
 $(document).keydown(function(event){
+	//event.preventDefault();//避免在移动时，滚动条也动，前提所有按键效果都消失，所以放在下面
 	switch(event.keyCode){
 		//left
 		case 37:
+			event.preventDefault();
 			if(moveLeft())
 			{
 				setTimeout("generateOneNumber()",210);
@@ -177,6 +183,7 @@ $(document).keydown(function(event){
 			break;
 		//up
 		case 38:
+			event.preventDefault();
 			if(moveUp())
 			{
 				setTimeout("generateOneNumber()",210);
@@ -185,6 +192,7 @@ $(document).keydown(function(event){
 			break;
 		//right
 		case 39:
+			event.preventDefault();
 			if(moveRight())
 			{
 				setTimeout("generateOneNumber()",210);
@@ -193,6 +201,7 @@ $(document).keydown(function(event){
 			break;
 		//down
 		case 40:
+			event.preventDefault();
 			if(moveDown())
 			{
 				setTimeout("generateOneNumber()",210);
@@ -203,6 +212,77 @@ $(document).keydown(function(event){
 			break;
 	}
 });
+
+//移动端touch
+//获取一开始的坐标
+document.addEventListener("touchstart",function(event){
+	startx=event.touches[0].pageX;//获取一个手指头的位置
+	starty=event.touches[0].pageY;
+});	
+
+//有些系统中使用preventDefault会导致touchstart和touchend无法使用，因此需添加touchmove
+document.addEventListener("touchmove",function(event){
+	event.preventDefault();
+})
+
+//获取离开屏幕的坐标
+document.addEventListener("touchend",function(event){
+	endx=event.changedTouches[0].pageX;
+	endy=event.changedTouches[0].pageY;
+
+	var deltax=endx-startx;
+	var deltay=endy-starty;
+
+	if(Math.abs(deltax)<0.3*documentWidth&&Math.abs(deltay)<0.3*documentWidth)//避免与点击混淆
+	{
+		return;
+	}
+	//x轴
+	if(Math.abs(deltax)>=Math.abs(deltay))//取绝对值
+	{
+		if(deltax>0)
+		{
+			//moveright
+			if(moveRight())
+			{
+				setTimeout("generateOneNumber()",210);
+				setTimeout("isgameover()",300);
+			}
+		}
+		else
+		{
+			//moveleft
+			if(moveLeft())
+			{
+				setTimeout("generateOneNumber()",210);
+				setTimeout("isgameover()",300);
+			}
+		}
+	}
+	//y轴
+	else
+	{
+		if(deltay>0)//y轴向下为正
+		{
+			//movedown
+			if(moveDown())
+			{
+				setTimeout("generateOneNumber()",210);
+				setTimeout("isgameover()",300);
+			}
+		}
+		else
+		{
+			//moveup
+			if(moveUp())
+			{
+				setTimeout("generateOneNumber()",210);
+				setTimeout("isgameover()",300);
+			}
+		}
+	}
+});
+
 
 //判断是否游戏结束
 function isgameover(){
