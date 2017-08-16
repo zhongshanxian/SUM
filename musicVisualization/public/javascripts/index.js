681ee3c35b673537c33b1acac624084d7d19636a
zhongshanxian <971296739@qq.com>
2017-08-16 22:28:54 +0800

    uodate

--- musicVisualization/public/javascripts/index.js
+++ musicVisualization/public/javascripts/index.js
@@ -40,11 +40,13 @@ function getDots(){
 	{
 		var x=random(0,width);
 		var y=random(0,height);
-		var color="rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")";
+		var color="rgba("+random(0,255)+","+random(0,255)+","+random(0,255)+",0)";
 		Dots.push({
 			x:x,
 			y:y,
-			color:color
+			dx:random(1,3),
+			color:color,
+			cap:0
 		});
 	}
 }
@@ -68,25 +70,40 @@ window.onresize=resize;
 function draw(arr){
 	ctx.clearRect(0,0,width,height);
 	var w=width/size;
+	var cw = w*0.7;
+	var capH = w*0.5;
 	ctx.fillStyle=line;
 	for(var i=0;i<size;i++)
-	{
+	{ 
+		var o=Dots[i];
 		if(draw.type=="column")
 		{
 			var h=arr[i] / 256;
 			h=h * height;
-			ctx.fillRect(w*i,height-h,w*0.6,h);
+			ctx.fillRect(w*i,height-h,cw,h);
+			ctx.fillRect(w*i,height-(o.cap+capH),cw,capH);
+			o.cap--;
+			if(o.cap<0)
+			{
+				o.cap=0;
+			}
+			if(h>0 && o.cap<h+40)
+			{
+				o.cap = h+40 > height-capH?height-capH:h+40;
+			}
 		}
-		else {
+		else if(draw.type=="dot"){
 			ctx.beginPath();
-			var o=Dots[i];
-			var r=arr[i]/256*50;
+			
+			var r=10 + arr[i]/256*(height>width?width:height)/15;
 			ctx.arc(o.x,o.y,r,0,Math.PI*2,true);
 			var g=ctx.createRadialGradient(o.x,o.y,0,o.x,o.y,r);
 			g.addColorStop(0,"white");
 			g.addColorStop(1,o.color);
 			ctx.fillStyle=g;
 			ctx.fill();
+			o.x+=o.dx;
+			o.x=o.x>width?0:o.x;
 		}
 	}
 }
