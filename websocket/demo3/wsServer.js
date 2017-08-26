0509cb0dc2c0200b17ad10de0967872d24467ed2
zhongshanxian <971296739@qq.com>
2017-08-26 22:52:24 +0800

    update

--- websocket/demo3/wsServer.js
+++ websocket/demo3/wsServer.js
@@ -1,15 +1,21 @@
 var ws = require("nodejs-websocket")
 
 var port = 3000;
+
+var clientCount = 0;
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
-	console.log("New connection")
+	console.log("New connection");
+	clientCount++;
+	conn.nickname = 'user'+clientCount;
+	broadcast(conn.nickname+ " comes in");
 	conn.on("text", function (str) {
-		console.log("Received "+str)
-		conn.sendText(str);
+		console.log("Received "+str);
+		broadcast(str);
 	});
 	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
+		console.log("Connection closed");
+		broadcast(conn.nickname+" left");
 	});
 	conn.on("error", function (err) {
 		console.log("handled error");
@@ -18,3 +24,9 @@ var server = ws.createServer(function (conn) {
 }).listen(port);
 
 console.log("websocket sercer listening on port: "+port);
+
+function broadcast(str)　{
+	server.connections.forEach(function(connection){
+		connection.sendText(str);
+	});
+}
