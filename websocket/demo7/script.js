0e9cde4e58b755ed8fb6ad504270e689a1ae4355
zhongshanxian <971296739@qq.com>
2017-08-29 23:11:54 +0800

    update

--- websocket/demo7/script.js
+++ websocket/demo7/script.js
@@ -25,4 +25,19 @@ var gameData = [
 	[0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0]
-]
+];
+var nextDivs = [];
+var gameDivs = [];
+var initGame = function () {
+	for(var i=0;i<gameData.length;i++) {
+		var gameDiv =[];
+		for (var j=0;j<gameData[0].length;j++) {
+			var newNode = document.createElement('div');
+			newNode.className = 'none';
+			newNode.style.top=(i*20)+'px';
+			newNode.style.left=(j*20)+'px';
+			document.getElementById('game').appendChild(newNode);
+			gameDiv.push(newNode);
+		}
+	}
+}
