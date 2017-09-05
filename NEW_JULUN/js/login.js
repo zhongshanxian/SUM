1af7a3f8298a66474d310698e006fe2c9136d65e
zhongshanxian <971296739@qq.com>
2017-09-05 23:49:04 +0800

    update

--- NEW_JULUN/js/login.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//点击确定后获取memberMobile和memberPasswd
-$("#registerBtn").click(function(){
-	console.log($("#memberMobile").val());
-	console.log($("#memberPasswd").val());
-});
--- NEW_JULUN/js/register.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-$("#registerBtn").click(function(){
-	console.log($("#memberMobile").val());
-	console.log($("#memberPasswd").val());
-	console.log($("#vaildCode").val());
-});
